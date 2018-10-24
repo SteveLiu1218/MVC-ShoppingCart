25bdd1576e1277c18edc05dd4491148059a081b0
SteveLiu1218 <m1992612@gmail.com>
2018-10-24 17:33:47 +0800

    CreatProductList
    Create CreateAction and Add EF

--- ShoppingCartPractice/Controllers/ProductListController.cs
+++ ShoppingCartPractice/Controllers/ProductListController.cs
@@ -8,11 +8,34 @@ namespace ShoppingCartPractice.Controllers
 {
     public class ProductListController : Controller
     {
+        //Models.CartsEntities db = new Models.CartsEntities();
         // GET: ProductList
         public ActionResult Index()
         {
+            List<Models.Carts> result = new List<Models.Carts>();
+            
+            using (Models.CartsEntities db = new Models.CartsEntities())
+            {
+                result = db.Carts.Select(c => c).ToList();
+            }
+
             var productLists = Models.ProductLists.GetProductLists();
-            return View(productLists);
+            return View(result);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Models.Carts productList)
+        {
+            using (Models.CartsEntities db = new Models.CartsEntities())
+            {
+                db.Carts.Add(productList);
+                db.SaveChanges();
+            }
+            return View();
         }
     }
 }
