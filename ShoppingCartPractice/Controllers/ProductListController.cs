a6141c5cbc3528aca1eeb95f795cb3b33df1cdb1
SteveLiu1218 <m1992612@gmail.com>
2018-10-24 14:15:39 +0800

    ShowProductList
    Basic MVC Working

---
+++ ShoppingCartPractice/Controllers/ProductListController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCartPractice.Controllers
+{
+    public class ProductListController : Controller
+    {
+        // GET: ProductList
+        public ActionResult Index()
+        {
+            var productLists = Models.ProductLists.GetProductLists();
+            return View(productLists);
+        }
+    }
+}
